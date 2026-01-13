8b69e03e7e9f3860e9f23860673bb862759f1835
gilcllys <gilcllyscosta@gmail.com>
2026-01-13 17:26:09 -0300

    feat: adicionar suporte a localização em português; atualizar componentes de transações com novos campos e estilos; melhorar responsividade e layout

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,6 +3,10 @@ import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
 import {
@@ -10,6 +14,7 @@ import {
   withEventReplay,
 } from '@angular/platform-browser';
 
+registerLocaleData(localePt); // Registra os dados de localidade do Brasil
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
@@ -17,5 +22,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideNativeDateAdapter(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
   ],
 };
